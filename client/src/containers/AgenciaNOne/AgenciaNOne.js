8b60a881adc81ea1c74a1b4560bd89d614cd7c6b
eron c <deandradeeron@gmail.com>
2020-01-30 09:11:38 -0300

    Fix

--- client/src/containers/AgenciaNOne/AgenciaNOne.js
+++ client/src/containers/AgenciaNOne/AgenciaNOne.js
@@ -9,44 +9,8 @@ class AgenciaNOne extends Component {
 render () {
     return ( 
         <div className={classes.AgenciaNOne}>
-            <h2>Comentários do Exercício (readme):</h2>
-            <p>Oi pessoal, meu nome é Eron Oliveira. Sou mineiro, tenho 30 anos e moro no Flamengo - Rio de Janeiro. O desafio proposto é refererente a DESIGN RESPONSIVO, mas como meu estudo é de ReactJS resolvi usá-lo, como pretendo fazer daqui em diante.</p>
-            <p>Meu github: <a href="https://github.com/kaiknux">https://github.com/kaiknux</a></p>
-            <p>Repositório deste desafio: <a href="https://github.com/kaiknux/agencia-n1-app">https://github.com/kaiknux/agencia-n1-app</a></p>
-            <p>Este desafio hospedado no Netlify: <a href="https://serene-yonath-2e29cf.netlify.com">https://serene-yonath-2e29cf.netlify.com</a></p>
-            <p>Meu site pessoal (work in progress): <a href="https://zealous-yonath-d830ee.netlify.com">https://zealous-yonath-d830ee.netlify.com</a></p>
-            <p>Desenvolvido no VSCode, versionado no Github.</p>
-            <p>Meu e-mail: eron.adco@gmail.com Skype: eron.adco@gmail.com. Telefone(whatsapp e telegram): (21)9 8228-7075</p>
-            <h2>Comentários do ReacTJS:</h2>
-            <h1>Posição almejada na Agencia N1</h1>
-            <p>Estudei a empresa de vocês, gostei e me interesso pelo que vocês fazem. Quero trabalhar com vocês - embora com pouca experiência em programação web busco as melhores práticas de programação e conhecimeto para melhor ajudar.</p>
-            <h1>Reaproveitamento de componentes X hard coding</h1>
-            <p>Me propus a fazer os componentes de ReactJS porque meu objetivo profissional para os próximos 3 anos é ganhar experiência e o máximo de conhecimento possível em NodeJS, ReactJS, React Native.</p>
-            <p>Componentes de backdrop, modal, sidedrawer, axios e Firebase são reaproveitados.</p>
-            <p>A proposta foi de fazer em 'hard code' o máximo possível, a fim de me expor ao teste do exercício. Não almejo a vaga 'chefão', mas busco isso no ReactJS: desenvolvimento na área de programação web.</p>
-            <p>Tenho consciência de que a solução do campo de busca não é a melhor possível e também coloquei pouquíssimas regras nos forms dado o prazo e contexto do desafio.</p>
-            <p>Acredito que a melhor solução para o desafio seria desenvolver com Redux ou Context API desde o inicio, porém prefiri trabalhar com jQuery por ter mais experiência e mais previsibilidade com essa ferramenta.</p>
-            <p>À medida em que tiver mais experiência pretendo melhorar este projeto a fim de compor meu portifólio.</p>
-            <h2>Comentários do CSS:</h2>
-            <h1>Cores dos tons de cinza (e demais cores)</h1>
-            <p>Usei o conta-gotas do Adobe Photoshop para estimar cada tom de cinza proposto pelo exercício.</p>
-            <h1>Fonte e font-weight dos botões 'COMPRA AE' e 'CALCULAR'</h1>
-            <p>Percebi que os botões 'COMPRA AE' e 'CALCULAR' do meu app não tem o mesmo font-weight e aspecto proposto pelo exercício, porém não consegui pensar de maneira rápida em
-                uma solução de 'font', 'font-weight' ou algo que igualasse ao proposto pelo exercício.</p>
-            <h2>App features:</h2>
-            <h1>Cadastro de novos presentes</h1>
-            <p>Não foram feitas regras para os inputs.</p>
-            <h1>Presentes</h1>
-            <p>Banco de dados simples no Firebase. Configuração mínima do Firebase, não há feature de delete.</p>
-            <h1>Modal</h1>
-            <p>Modal renderizando props children. 3 possibilidades de modal: Mensagem para compra realizada (desafio), mensagem de aviso que não fiz
-                 feature de cálculo de frete e mensagem de aviso de carrinho desativado.</p>
-            <h1>"Presentes" relacionados</h1>
-            <p>Componente reaproveitado do github. Por se tratar de um projeto desenvolvido em 1 semana - o banco de dados não apresenta categorização de produtos, portanto o componente não passa por demais
-                avaliações pré renderização do mesmo.</p>
-            <h1>Breadcrumbs</h1>
-            <p> Não responsivo, só texto </p>
-        </div>
+            Teste CRUD em e-commerce
+            </div>
     )
 }
 }
